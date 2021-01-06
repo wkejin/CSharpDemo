90cc6a6d82beffb7309618a3119205b8046d7e1c
Kejin Weng <emiyaz@outlook.com>
2021-01-06 17:16:43 +0800

    loop

--- Demo02_流程控制/Program.cs
+++ Demo02_流程控制/Program.cs
@@ -6,19 +6,28 @@ namespace Demo02_流程控制
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("请输入一个整数：");
-            int intNum = Convert.ToInt32(Console.ReadLine());
-            if(intNum > 0)
+            // Console.WriteLine("请输入一个整数：");
+            // int intNum = Convert.ToInt32(Console.ReadLine());
+            // if(intNum > 0)
+            // {
+            //     Console.WriteLine($"你输入了{intNum}，是一个正数。");
+            // }
+            // else if(intNum < 0)
+            // {
+            //     Console.WriteLine($"你输入了{intNum}, 是一个负数。");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("你输入了0。");
+            // }
+
+            for (int i = 1; i <= 9; i++)
             {
-                Console.WriteLine($"你输入了{intNum}，是一个正数。");
-            }
-            else if(intNum < 0)
-            {
-                Console.WriteLine($"你输入了{intNum}, 是一个负数。");
-            }
-            else
-            {
-                Console.WriteLine("你输入了0。");
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{i}*{j}={i*j}\t");
+                }
+                Console.WriteLine();
             }
         }
     }
