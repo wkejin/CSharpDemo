97af326365b19f38476e2a3d9cbb6595c89ae879
Kejin Weng <emiyaz@outlook.com>
2021-01-06 16:31:36 +0800

    print a new message

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -7,6 +7,7 @@ namespace Demo01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm learning the C#.");
         }
     }
 }
