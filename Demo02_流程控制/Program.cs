2cc1ebfb9ce4919c3ea3129a864a08d1d17dad51
Kejin Weng <emiyaz@outlook.com>
2021-01-06 17:01:28 +0800

    学习流程控制

---
+++ Demo02_流程控制/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Demo02_流程控制
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个整数：");
+            int intNum = Convert.ToInt32(Console.ReadLine());
+            if(intNum > 0)
+            {
+                Console.WriteLine($"你输入了{intNum}，是一个正数。");
+            }
+            else if(intNum < 0)
+            {
+                Console.WriteLine($"你输入了{intNum}, 是一个负数。");
+            }
+            else
+            {
+                Console.WriteLine("你输入了0。");
+            }
+        }
+    }
+}
