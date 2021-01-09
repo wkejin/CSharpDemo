f5b8131f4ee673151f9c155c0545070f52a7316d
Kejin Weng <emiyaz@outlook.com>
2021-01-09 14:05:23 +0800

    枚举

---
+++ Demo03_枚举/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Demo03_枚举
+{
+    /// <summary>
+    /// 声明了一个名为QQState的枚举类型，各个值对应的int数值默认从0开始递增
+    /// </summary>
+    enum QQState
+    {
+        Online,
+        Offline,
+        Leave,
+        Busy,
+        QMe
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用枚举
+            Console.WriteLine(QQState.Online);
+            //枚举类型强制转换为int
+            Console.WriteLine((int)QQState.Leave);
+            //int强制转换为枚举类型
+            Console.WriteLine((QQState)1);
+        }
+    }
+}
