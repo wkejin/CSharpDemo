51537bb05a775013cbe7bc4fe9c28138d8d9498e
Kejin Weng <emiyaz@outlook.com>
2021-01-09 14:35:33 +0800

    结构

---
+++ Demo04_结构/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Demo04_结构
+{
+    /// <summary>
+    /// 声明了名为Student的结构类型
+    /// </summary>
+    public struct Student
+    {
+        public int _id;
+        public string _name;
+        public int _age;
+
+        public void Say()
+        {
+            Console.WriteLine($"我是学生，名叫{_name},我今年{_age}岁。");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用结构类型
+            Student stuZS;
+            stuZS._id = 1;
+            stuZS._name = "张三";
+            stuZS._age = 18;
+
+            stuZS.Say();
+        }
+    }
+}
