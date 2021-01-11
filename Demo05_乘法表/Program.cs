b0ceeecaf3731283737d89b14270dbc8419dc4d9
Kejin Weng <emiyaz@outlook.com>
2021-01-11 12:47:22 +0800

    循环嵌套输出乘法表

---
+++ Demo05_乘法表/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Demo05_乘法表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{i}*{j}={i*j}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
