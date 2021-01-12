4e65cee5a4114183db0247bfc69134a7be91322f
Kejin Weng <emiyaz@outlook.com>
2021-01-12 14:06:07 +0800

    params

---
+++ Demo10_可变参数params/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo10_可变参数params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            OutputArr(a);
+            int[] nums = {1, 2, 3, 4, 5};
+            OutputArr(nums);
+        }
+
+        private static void OutputArr(params int[] nums)
+        {
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
