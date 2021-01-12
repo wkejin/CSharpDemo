97039008a17887f585a5fd956c020bd6bfc4ea90
Kejin Weng <emiyaz@outlook.com>
2021-01-12 14:11:31 +0800

    params

--- Demo10_可变参数params/Program.cs
+++ Demo10_可变参数params/Program.cs
@@ -10,8 +10,13 @@ namespace Demo10_可变参数params
             OutputArr(a);
             int[] nums = {1, 2, 3, 4, 5};
             OutputArr(nums);
+            OutputArr(12,34,56);
         }
 
+        /// <summary>
+        /// 方法的参数不一定只有一个，params修饰的参数必须是最后一个
+        /// </summary>
+        /// <param name="nums"></param>
         private static void OutputArr(params int[] nums)
         {
             foreach (var num in nums)
