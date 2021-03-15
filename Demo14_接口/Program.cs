7e9e487e6bdfe20afab9952d3a2252ad93da764b
Kejin Weng <emiyaz@outlook.com>
2021-03-15 23:17:49 +0800

    接口

---
+++ Demo14_接口/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo14_接口
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public  void CHLSS()
+        {
+            Console.WriteLine("我是人类，我可以吃喝拉撒睡。");
+        }
+    }
+}
