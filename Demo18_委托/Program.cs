21b073489a0c4d62d1f2cf673fae3f2cace144de
Kejin Weng <emiyaz@outlook.com>
2021-03-24 18:45:54 +0800

    委托基本使用

---
+++ Demo18_委托/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Demo18_委托
+{
+    delegate int Max(int[] nums);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 5, 2, 7, 4, 10, 8 };
+            Max m = new Max(GetMax);
+            int max = m(numbers);
+            Console.WriteLine(max);
+        }
+
+        static int GetMax(int[] nums)
+        {
+            int max = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                }
+            }
+            return max;
+        }
+    }
+}
