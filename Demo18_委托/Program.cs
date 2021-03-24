bf5fe97635f0c5fbcc259509b0af6cd9b5596b3d
Kejin Weng <emiyaz@outlook.com>
2021-03-25 07:08:52 +0800

    委托基本使用

--- Demo18_委托/Program.cs
+++ Demo18_委托/Program.cs
@@ -1,16 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Demo18_委托
 {
-    delegate int Max(int[] nums);
+    public delegate int Max(int[] nums);
     class Program
     {
         static void Main(string[] args)
         {
             int[] numbers = { 1, 5, 2, 7, 4, 10, 8 };
-            Max m = new Max(GetMax);
+            //Max m = new Max(GetMax);
+            Max m = GetMax;
             int max = m(numbers);
             Console.WriteLine(max);
+
+            Calc calc = new Calc();
+            Action act = calc.Report;
+            act();
+            act.Invoke();
+            Func<double, double, double> f1 = calc.Add;
+            Func<double, double, double> f2 = calc.Mul;
+            Console.WriteLine(f1(1, 2));
+            Console.WriteLine(f2(1.3d, 2.2d));
         }
 
         static int GetMax(int[] nums)
@@ -24,6 +35,23 @@ namespace Demo18_委托
                 }
             }
             return max;
+        }
+    }
+
+    class Calc
+    {
+        public void Report()
+        {
+            Console.WriteLine("I have 4 methods.");
+        }
+
+        public double Add(double a, double b) => a + b;
+        public double Sub(double a, double b) => a - b;
+        public double Mul(double a, double b) => a * b;
+        public double Div(double a, double b)
+        {
+            double res = b != 0 ? a / b : 0;
+            return res;
         }
     }
 }
---
+++ Demo19_委托使用/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo19_委托使用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductFactory productFactory = new ProductFactory();
+            WrapFactory wrapFactory = new WrapFactory();
+
+            Func<Product> func1 = productFactory.MakePizza;
+            Func<Product> func2 = productFactory.MakeToyCar;
+
+            Box box1 = wrapFactory.WrapProduct(func1);
+            Box box2 = wrapFactory.WrapProduct(func2);
+
+            Console.WriteLine(box1.Product.ProductName);
+            Console.WriteLine(box2.Product.ProductName);
+        }
+    }
+
+    class Product
+    {
+        public string ProductName { get; set; }
+    }
+
+    class Box
+    {
+        public Product Product { get; set; }
+    }
+
+    class WrapFactory
+    {
+        public Box WrapProduct(Func<Product> getProduct)
+        {
+            Box box = new Box();
+            box.Product = getProduct.Invoke();
+            return box;
+        }
+    }
+
+    class ProductFactory
+    {
+        public Product MakePizza()
+        {
+            Product product = new Product();
+            product.ProductName = "Pizza";
+            return product;
+        }
+
+        public Product MakeToyCar()
+        {
+            Product product = new Product();
+            product.ProductName = "Toy Car";
+            return product;
+        }
+    }
+
+}
